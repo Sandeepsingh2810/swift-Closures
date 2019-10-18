7ac553da0cb6f909d24e2eceb556696956719aac
sandeepsingh2810 <sandeepjangra0100@gmail.com>
2019-10-18 19:21:13 -0400

    desc and asce functions

--- swift Closures/main.swift
+++ swift Closures/main.swift
@@ -5,7 +5,8 @@
 //  Created by MacStudent on 2019-10-18.
 //  Copyright © 2019 MacStudent. All rights reserved.
 //
-
+//IMPORTANT FOR FINAL EXAM
+// all the closures are reference type
 import Foundation
 
 //Closures
@@ -19,10 +20,50 @@ let result = divide(10,2)
 print(result)
 
 //2.expression Example
-func concatStr(s1: String, s2: String) -> String{
-    let val = String(format: "%@ %@",s1,s2)
+let concatStr = {
+            (s1: String, s2: String) -> String in
+
+    let val = String(format: "%@ %@", s1, s2)
     return val
-    
 }
-let finalresult = concatStr(s1: "Toronto", s2: "City")
-print(finalresult)
+let myFunc: (String,String) -> String // defination of the func.,high ordwer func=passing func into another func
+
+myFunc = concatStr
+
+let myResult = concatStr("Toronto", "City")
+print(myResult)
+
+//3.Single Expression Implisit
+var myArray:[Int] = [5,6,70,50,]
+let sum = myArray.map {(x) -> Int in
+    return x+1
+
+}
+print(sum)
+
+var desc = myArray.sorted(by: { (n1,n2) -> Bool in
+return n1 > n2 // one line
+} )
+print(desc)
+
+//ascending
+
+var Array:[Int] = [100,1,8,200,]
+
+let asce = Array.sorted(by: { (n1,n2) -> Bool in
+    return n1 < n2
+} )
+print(asce)
+
+//desc = myArray.sorted(by: {
+//    return $0 > $1 // one line
+//})
+//print(desc)
+
+//desc = myArray.sorted(by: >)
+//print(desc)
+
+
+
+
+
